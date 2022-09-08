4147fc0048347e1dfdd7aa048409bad1a9e98df7
acopperlily <acopperlily@yahoo.com>
2022-09-08 01:28:59 -0400

    Solve addItem.js

---
+++ addItem.js
@@ -0,0 +1,15 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+/* PREP
+  Parameters: well, it's not really a function
+  Returns: an array containing the element 'codewars'
+  Example: a console.log statement would show the variable websites to return ['codewars']
+  Pseudocode: use the push method to add 'codewars' to empty array
+*/
+
+const websites = [];
+websites.push('codewars');
+console.log(websites);
